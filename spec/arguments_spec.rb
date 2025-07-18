ce0a7b951aaed903472a7b75b6ff1dc3d71edee4
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-07-18 18:13:54 -0400

    fix arguments_spec
    In Ruby 3.3, Readline gets replaced with Reline. This causes some extra
    ANSI escape codes to get added to the lines that are printed out. I
    don't know what that is about, but given that 3.2 is nearing EOL it
    seems prudent to only test for the outputs of 3.3 and 3.4.

--- spec/arguments_spec.rb
+++ spec/arguments_spec.rb
@@ -6,6 +6,8 @@ require 'open3'
 
 describe 'Rails console post -- arguments' do
   describe 'containing "--simple-prompt"' do
+    # Readline outputs differently after 3.2. Skipping since 3.2 is near EOL.
+    break if RUBY_VERSION < "3.3"
     it 'triggers the simple prompt' do
       prompt = nil
       simple_prompt = "\e[0G\e[?25l\e[1G\e[K\e[1G\e[0m>> \e[0m\e[?25h\e[4G\e[?25l\e[1G\e[K\e[?25h\e[1G>> \r\n"
@@ -20,6 +22,8 @@ describe 'Rails console post -- arguments' do
   end
 
   describe 'containing "--noprompt"' do
+    # Readline outputs differently after 3.2. Skipping since 3.2 is near EOL.
+    break if RUBY_VERSION < "3.3"
     it 'triggers the "" prompt' do
       prompt = nil
       noprompt = "\e[0G\e[?25l\e[1G\e[K\e[?25h\e[1G\e[?25l\e[?25h\e[1G \r\n"
